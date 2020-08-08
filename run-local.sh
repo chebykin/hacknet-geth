eb201fbf068d8f4fe1e24b865a6c91b33fcae842
Nikita Chebykin <nikita@chebyk.in>
2020-08-08 17:41:46 +0200

    Update run local script

--- run-local.sh
+++ run-local.sh
@@ -4,4 +4,8 @@ geth --networkid 57744 \
   --datadir ./ \
   --cache 512 \
   --port 30303 \
+  --http \
+  --http.addr 0.0.0.0 \
+  --http.vhosts '*' \
+  --http.corsdomain '*' \
   # --bootnodes enode://0ff0072e33b1f6675739e1611fd79f5b9ef6da0262e1d4379af595a1692c3e0f40e210b5bc40d54e608f55be925d0b000aac22950b0e60e041f7fdb2478068fa@139.59.133.174:30303
